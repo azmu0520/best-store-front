d7beffa331e592127b5805b3c8de02f20b82dc95
sardor.mukhtorov <sardor.mukhtorov@inoviagroup.se>
2021-06-29 19:16:02 +0500

    navbar is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Header from "./Components/Header";
-
+import Navbar from "./Components/Navbar";
 function App() {
   return (
     <div>
       <Header />
+      <Navbar />
     </div>
   );
 }
